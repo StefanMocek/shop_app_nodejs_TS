c499e45d7c4a696723442d9bc8e5ecffdc655434
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-15 22:55:35 +0100

    seller router continued

--- src/seller/seller.routers.ts
+++ src/seller/seller.routers.ts
@@ -1,9 +1,27 @@
 import {NextFunction, Request, Response, Router} from 'express';
+import { BadRequestError, Uploader, UploaderMiddlewareOptions } from '@shop-app-package/common';
 
 const router = Router();
 
-router.post('/product/new', async (req: Request, res: Response, next: NextFunction) => {
+const uploader = new Uploader();
+const middlewareOptions: UploaderMiddlewareOptions = {
+  types: ['image/png', 'image/jpeg'],
+  fieldName: 'image'
+};
+
+const multipleFilesMiddleware = uploader.uploadMultipleFiles(middlewareOptions);
+
+router.post('/product/new',multipleFilesMiddleware, async (req: Request, res: Response, next: NextFunction) => {
   const {title, price} = req.body;
+
+  if(!req.files) {
+    return next(new BadRequestError('images are required'))
+  }
+  
+  if(req.uploaderError) {
+    return next(new BadRequestError(req.uploaderError.message));
+  };
+
   
 }) 
 
