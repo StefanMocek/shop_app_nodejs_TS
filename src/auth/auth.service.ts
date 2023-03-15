6237299449b93863860264975706dc6ff3fe1a97
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-15 15:47:13 +0100

    signin method and signup fixed

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,6 @@
-import {NextFunction} from "express";
-import {CreateUserDto} from "./dtos/auth.dto";
+import {AuthDto} from "./dtos/auth.dto";
 import {UserService, userService} from "./user/user.service";
-import {BadRequestError, AuthenticationService} from "@shop-app-package/common";
+import {AuthenticationService} from "@shop-app-package/common";
 
 
 export class AuthService {
@@ -10,15 +9,31 @@ export class AuthService {
     public authenticationService: AuthenticationService 
   ) {}
 
-  async signup (createUserDto: CreateUserDto, errCallback: NextFunction) {
+  async signup (createUserDto: AuthDto) {
     const existingUser = await this.userService.findOneByEmail(createUserDto.email);
     if (existingUser) {
-      return errCallback(new BadRequestError('User with the same email already exists'))
+      return {message: 'Email already taken'}
     };
     const newUser = await this.userService.create(createUserDto);
 
     const jwt = this.authenticationService.generateJwt({email: createUserDto.email, userId: newUser.id}, process.env.JWT_KEY!)
-    return jwt;
+    return {jwt};
+  };
+
+  async signin(signInDto:AuthDto) {
+    const user = await this.userService.findOneByEmail(signInDto.email);
+    if (!user) {
+      return {message: 'Wrong credentials'}
+    };
+
+    const pwdCompered = this.authenticationService.pwdCompare(user.password, signInDto.password);
+
+    if (!pwdCompered) {
+      return {message: 'Wrong credentials'}
+    };
+
+    const jwt = this.authenticationService.generateJwt({email: user.email, userId: user.id}, process.env.JWT_KEY!)
+    return {jwt};  
   }
 }
 export const authService = new AuthService(userService, new AuthenticationService());
