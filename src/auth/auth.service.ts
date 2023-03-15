8ec2981c982985b7840c195f0e4e050ebc69c743
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-15 11:17:42 +0100

    signup method finished

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,13 @@
 import {NextFunction} from "express";
 import {CreateUserDto} from "./dtos/auth.dto";
 import {UserService, userService} from "./user/user.service";
-import {BadRequestError} from "@shop-app-package/common";
+import {BadRequestError, AuthenticationService} from "@shop-app-package/common";
 
 
 export class AuthService {
   constructor (
-    public userService: UserService
+    public userService: UserService,
+    public authenticationService: AuthenticationService 
   ) {}
 
   async signup (createUserDto: CreateUserDto, errCallback: NextFunction) {
@@ -14,7 +15,10 @@ export class AuthService {
     if (existingUser) {
       return errCallback(new BadRequestError('User with the same email already exists'))
     };
-    const newuser = await this.userService.create(createUserDto)
+    const newUser = await this.userService.create(createUserDto);
+
+    const jwt = this.authenticationService.generateJwt({email: createUserDto.email, userId: newUser.id}, process.env.JWT_KEY!)
+    return jwt;
   }
 }
-export const authService = new AuthService(userService);
+export const authService = new AuthService(userService, new AuthenticationService());
