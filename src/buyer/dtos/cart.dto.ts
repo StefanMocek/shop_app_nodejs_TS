aacad3c070a5d67bac79e98432b3ce2aa9ca66be
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-20 18:02:06 +0100

    cart dtos

---
+++ src/buyer/dtos/cart.dto.ts
@@ -0,0 +1,11 @@
+export interface AddProductToCartDto {
+  userId: string,
+  quantity: number,
+  productId: string
+};
+
+export interface CreateCartProductDto {
+  cartId: string,
+  quantity: number,
+  productId: string
+}
