a5361e0a588bc73a221c53f58d217c08a44d7ee1
Stefan Mocek Polska <etktuzudu@gmail.com>
2023-03-21 20:39:18 +0100

    checkOut method init

--- src/buyer/buyer.service.ts
+++ src/buyer/buyer.service.ts
@@ -1,12 +1,17 @@
 import {BadRequestError, NotAuthorizedError} from "@shop-app-package/common";
 import {ProductService, productService} from "../seller/product/product.service";
 import {CartService, cartService} from "./cart/cart.service";
-import {AddProductToCartDto, RemoveProductFromCartDto, UpdateCartProductQuantityDto} from "./dtos/cart.dto";
+import {
+  AddProductToCartDto, 
+  RemoveProductFromCartDto, 
+  UpdateCartProductQuantityDto} from "./dtos/cart.dto";
+import Stripe from 'stripe'
 
 export class BuyerService {
   constructor(
     public cartService: CartService,
-    public productService: ProductService 
+    public productService: ProductService,
+    public stripeService: Stripe
   ){}
 
   async addProductToCart(addProductToCartDto: AddProductToCartDto){
@@ -65,6 +70,41 @@ export class BuyerService {
 
     return cart;
   };
+
+  async checkOut(userId: string, cardToken: string, userEmail: string){
+    const cart = await this.cartService.findOneByUserId(userId);
+    if(!cart || cart.products.length === 0) {
+      return new BadRequestError('Your cart is empty');
+    };
+
+    const {id} = await this.stripeService.customers.create({
+      email: userEmail,
+      source: cardToken
+    });
+    if(!id) {
+      return new BadRequestError('Invalid data');
+    };
+
+    const charge = await this.stripeService.charges.create({
+      amount: cart.totalPrice * 100,
+      currency: 'pln',
+      customer: id
+    });
+
+    if(!charge) {
+      return new BadRequestError('Invalid data. Couldnt create a charge');
+    };
+
+    // create new order
+
+    // clear cart
+
+    return charge;
+  }
 };
 
-export const buyerService = new BuyerService(cartService, productService);
+export const buyerService = new BuyerService(
+  cartService, 
+  productService,
+  new Stripe(process.env.STRIPE_KEY!, {apiVersion: '2022-11-15'})
+);
